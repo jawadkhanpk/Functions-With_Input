680227eff58eb9570323d140be7567414d3e5552
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-16 18:34:11 +0500

    another commit

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ def greet_with(name, location):     # here name and location both are parameters
     print(f"Hello {name}")
     print(f"What is it like in {location}")
 
-greet_with(name="jawad",location="abbottabad")  # here jawad and abbottabad are the arguments of a function
+greet_with("jawad","abbottabad")  # here jawad and abbottabad are the arguments of a function
+# in line 9 jawad and abbottabad are also positional argument

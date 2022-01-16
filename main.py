69cbcdde5709a4c48b20afca3eedd28edc91cd16
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-16 18:31:20 +0500

    another commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def greet_with(name, location):
-    print(f"Hello this is {name}")
-    print(f"Your location is {location}")
 
-greet_with(name="jawad",location="abbottabad")
+# Parameters = name of the data
+# Arguments  = The actual data
+
+def greet_with(name, location):     # here name and location both are parameters of a function
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with(name="jawad",location="abbottabad")  # here jawad and abbottabad are the arguments of a function
